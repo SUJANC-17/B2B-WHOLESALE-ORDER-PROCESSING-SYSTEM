017325411520716ad5f93bc3afea3a7eecb0f110
SUJANC-17 <sujanc3306@gmail.com>
2025-12-15 13:21:12 +0000

    Mon Dec 15 2025 13:21:11

--- springapp/src/main/java/com/examly/springapp/model/Supplier.java
+++ springapp/src/main/java/com/examly/springapp/model/Supplier.java
@@ -1,9 +1,13 @@
 package com.examly.springapp.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Supplier {
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int Id;
 }
